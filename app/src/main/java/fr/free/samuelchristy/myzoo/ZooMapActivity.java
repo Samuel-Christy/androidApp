1f81f48aa3a77b641212285c3b8940efa6f03242
Sam <s4perlipopette@gmail.com>
2018-06-05 10:15:50 +0200

    1st activty

---
+++ app/src/main/java/fr/free/samuelchristy/myzoo/ZooMapActivity.java
@@ -0,0 +1,37 @@
+package fr.free.samuelchristy.myzoo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class ZooMapActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(new ZooMapActivityView(this)); //lance la vue
+    }
+
+    public class ZooMapActivityView extends View{
+
+        Bitmap laCarte;
+
+        public ZooMapActivityView(Context context) {
+            super(context);
+            laCarte = BitmapFactory.decodeResource(getResources(),R.drawable.zoo); //charge l'image
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(laCarte,0,0,null); //affiche l'image
+
+        }
+    }
+
+}
