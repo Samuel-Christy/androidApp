b08504e93759b18f73d3ee05547a9d7071a6c90f
Sam <s4perlipopette@gmail.com>
2018-06-07 11:47:20 +0200

    accueil

---
+++ app/src/main/java/fr/free/samuelchristy/myzoo/AccueilActivity.java
@@ -0,0 +1,13 @@
+package fr.free.samuelchristy.myzoo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AccueilActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
