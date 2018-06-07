de429b1b4cda61357e7a1c0b09059282e928429b
Sam <s4perlipopette@gmail.com>
2018-06-07 14:55:22 +0200

    alert

--- app/src/main/java/fr/free/samuelchristy/myzoo/AccueilActivity.java
+++ app/src/main/java/fr/free/samuelchristy/myzoo/AccueilActivity.java
@@ -1,8 +1,12 @@
 package fr.free.samuelchristy.myzoo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AccueilActivity extends Activity {
 
@@ -10,5 +14,29 @@ public class AccueilActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
+        TextView v = findViewById(R.id.accueil_textView_news);
+        v.setText("pouet pouet !");
+
+
+        final Button button = findViewById(R.id.accueil_button_map);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), ZooMapActivity.class);
+                //v.getContext() = AccueilActivity.this
+                startActivity(i);
+            }
+        });
+
+        final Button buttonAlertActivity = findViewById(R.id.accueil_button_alertActivity);
+        buttonAlertActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), AlertActivity.class);
+                //v.getContext() = AccueilActivity.this
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/fr/free/samuelchristy/myzoo/AlertActivity.java
@@ -0,0 +1,14 @@
+package fr.free.samuelchristy.myzoo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class AlertActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alert);
+    }
+}
